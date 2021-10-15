0eaec9d204d4062d8624444fe8dddedba0d58e16
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2021-10-15 17:38:34 -0300

    Integrando typecript

--- model/porta.ts
+++ model/porta.ts
@@ -1,11 +1,11 @@
-export default class porta {
-  #numero
-  #temPresente
-  #selecionada
-  #aberta
+export default class PortaModel {
+  #numero: number
+  #temPresente: boolean
+  #selecionada: boolean
+  #aberta: boolean
 
   constructor(
-    numero, 
+    numero: number, 
     temPresente = false, 
     selecionada = false, 
     aberta = false) {
@@ -33,16 +33,16 @@ export default class porta {
 
     desselecionar() {
       const selecionada = false
-      return new Porta(this.numero, this.temPresente, selecionada, this.aberta)
+      return new PortaModel(this.numero, this.temPresente, selecionada, this.aberta)
     }
 
     alternarSelecao() {
       const selecionada = !this.selecionada
-      return new Porta(this.numero, this.temPresente, selecionada, this.aberta)
+      return new PortaModel(this.numero, this.temPresente, selecionada, this.aberta)
     }
 
     abrir() {
       const aberta = true
-      return new Porta(this.numero, this.temPresente, this.selecionada, aberta)
+      return new PortaModel(this.numero, this.temPresente, this.selecionada, aberta)
     }
 }
