5c6629f34fc3503326ea529c95df7c87eb337f2c
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2021-10-15 18:50:50 -0300

    Adicionando evento a porta #03

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,9 @@ import PortaModel from '../model/porta';
 
 export default function Home() {
   const [porta, setPorta] = useState(new PortaModel(1))
-  const [texto, setTexto] = useState('...')
   
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <input type="text" value={texto}
-      onChange={event => setTexto(event.target.value)} />
       <Porta 
         value={porta} 
         onChange={novaPorta => setPorta(novaPorta)}
