2a878fd9e5f6defad50e1e7634885558490e6493
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2021-10-15 22:01:39 -0300

    Lidando com array de portas #02

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,24 @@
 import { useState } from 'react';
 import Porta from '../components/Porta';
+import { atualizarPortas, criarPortas } from '../functions/portas';
 import PortaModel from '../model/porta';
 
 export default function Home() {
-  const [porta, setPorta] = useState(new PortaModel(1))
+  const [portas, setPortas] = useState(criarPortas(3, 2))
+
+  function renderizarPortas() {
+    return portas.map(porta => {
+      return <Porta 
+      key={porta.numero}
+      value={porta} 
+      onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))}
+      />
+    })
+  }
   
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <Porta 
-        value={porta} 
-        onChange={novaPorta => setPorta(novaPorta)}
-        />
+    <div style={{ display: 'flex' }}>
+      {renderizarPortas()}
     </div>
   )
 }
